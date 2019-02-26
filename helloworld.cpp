18135e8837add117ac8b2e1e2cfe148cbbfb6887
hrx32122 <hrx32122@zwoho.com>
2019-02-26 15:22:32 -0500

    changed to columbia

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main(){
-    std::cout << "Hello, world!" << std::endl;
+    std::cout << "Hello, Columbia!" << std::endl;
     std::cout << "My name is Brandon" << std::endl;
     std::cout << "My name is AAAAAA" << std::endl;
     return 0;
