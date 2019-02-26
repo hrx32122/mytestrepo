06c48cbda67961adee6537e01406d724eee50727
hrx32122 <hrx32122@aaa.com>
2019-02-26 15:15:26 -0500

    changed to universe

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main(){
-    std::cout << "Hello, world!" << std::endl;
+    std::cout << "Hello, universe!" << std::endl;
     std::cout << "My name is Brandon" << std::endl;
     std::cout << "My name is AAAAAA" << std::endl;
     return 0;
